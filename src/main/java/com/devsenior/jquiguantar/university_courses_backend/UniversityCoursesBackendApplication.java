ccb8b3af2377bcffac8f257878839f9fd9da4182
Jeferson Quiguantar <jeffersonquiguantar@gmail.com>
2025-08-25 09:44:56 -0500

    Initial Commit

---
+++ src/main/java/com/devsenior/jquiguantar/university_courses_backend/UniversityCoursesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.devsenior.jquiguantar.university_courses_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityCoursesBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UniversityCoursesBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsenior/jquiguantar/university_courses_backend/UniversityCoursesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsenior.jquiguantar.university_courses_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityCoursesBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
